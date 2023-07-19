a31794140f0c46c6af9db0d7f5b7bf9a7fb8238d
A-Steeves <48775637+AlexSteeves@users.noreply.github.com>
2023-07-18 21:15:45 -0400

    com2

---
+++ BSTSort.java
@@ -0,0 +1,24 @@
+class BSTSort{
+
+    class Node {
+        Node right, left;
+        int key;
+
+        public Node(int key){
+            this.key = key;
+            left = right = null;
+        }
+
+
+    }
+
+    Node root;
+
+    BSTSort(){
+        root = null;
+    }
+
+    void insert(){
+        
+    }
+}
